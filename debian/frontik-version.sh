ac93a9584bc60967eb9e86de592535b2c6979111
Ivan Fedorov <oxyum@oxyum.ru>
2010-12-27 19:39:01 +0300

    frontik 2.5.4
    Version up for repo

--- debian/frontik-version.sh
+++ debian/frontik-version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-version=`dpkg-parsechangelog| grep Version | sed "s/^Version: \(.*\)/\1/"`
+version=`dpkg-parsechangelog | grep -m 1 "^Version:" | sed "s/^Version: \(.*\)/\1/"`
 sed -i "s/DEVELOPMENT/$version/" debian/frontik/usr/share/pyshared/frontik/version.py
