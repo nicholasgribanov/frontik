1e9f9ec8d6b70e5e36c88d5b43f99d5aa78c2b85
Andrey Sumin <sumin.andrew@gmail.com>
2019-06-19 08:55:46 +0300

    remove unneccessary logging

--- frontik/handler_active_limit.py
+++ frontik/handler_active_limit.py
@@ -14,12 +14,10 @@ handlers_count_logger = logging.getLogger('handlers_count')
 
 class ActiveHandlersLimit:
     count = 0
-    high_watermark_ratio = 0.75
 
     def __init__(self, statsd_client: 'Optional[StatsdClientWithTags]'):
         self._acquired = False
         self._statsd_client = statsd_client
-        self._high_watermark = int(options.max_active_handlers * self.high_watermark_ratio)
 
         if ActiveHandlersLimit.count > options.max_active_handlers:
             handlers_count_logger.warning(
@@ -27,12 +25,6 @@ class ActiveHandlersLimit:
             )
 
             raise HTTPError(503)
-
-        elif ActiveHandlersLimit.count > self._high_watermark:
-            handlers_count_logger.warning(
-                'active handlers count reached %.2f * %s watermark (%s)',
-                self.high_watermark_ratio, options.max_active_handlers, ActiveHandlersLimit.count
-            )
 
         self.acquire()
 
